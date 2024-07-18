d06d5bbd394c74f8838466b6139b37b18fe75403
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-19 00:49:20 +0500

    Add reset grid button handler

--- javascript.js
+++ javascript.js
@@ -3,8 +3,10 @@ let gridDimension=gridContainer.offsetWidth;
 console.log(gridDimension);
 let gridSize=2;
 let divRef;
+let coloring=false;
 function clickedNewGridButton(event)
 {
+    coloring=false;
     gridContainer.textContent="";
     gridSize=prompt("Enter value of n for a n x n grid");
     if(gridSize>100)
@@ -26,24 +28,38 @@ function clickedNewGridButton(event)
     const grid=document.querySelectorAll(".grid");
     grid.forEach(function(item)
     {
-        item.addEventListener("click",clickedGrid);        
+        item.addEventListener("click",clickedGrid);
+        item.addEventListener("mouseenter",hoverGrid);        
         
     });
 }
 function clickedGrid(event)
 {
-    console.log(event);
-    event.target.addEventListener("mouseenter",hoverGrid);
+    event.target.style.background="black";
+    coloring=!coloring;
     
 }
 function hoverGrid(event)
 {
-    event.target.style.background="black";
+    if(coloring)
+        event.target.style.background="black";
+}
+function clickedResetGridButton()
+{
+    const grid=document.querySelectorAll(".grid");
+    grid.forEach(function(item)
+    {
+        item.style.background="#EA738D";     
+        
+    });    
+    coloring=false;
 }
 
-
 const newGridButton=document.querySelector("#newGridButton");
+const resetGridButton=document.querySelector("#resetGridButton");
+
 newGridButton.addEventListener("click",clickedNewGridButton);
+resetGridButton.addEventListener("click",clickedResetGridButton);
 
 
 
