f735a14726febe990eb1b61ab8922c969c2f196b
UzairKhan-27 <m.uzairkhan1000@gmail.com>
2024-07-18 18:52:47 +0500

    Fix Grid Dimensions
    Grids now fully occupy the gridContainer

--- javascript.js
+++ javascript.js
@@ -1,14 +1,17 @@
 const gridContainer=document.querySelector("#gridContainer");
-const gridSize=16;
+let gridDimension=gridContainer.offsetWidth;
+console.log(gridDimension);
+const gridSize=15;
 for(let i=0;i<gridSize;i++)
 {
     for(let j=0;j<gridSize;j++)
     {
         const divRef=document.createElement("div");
         divRef.classList.add("grid");
+        divRef.style.width=`${gridDimension/gridSize}px`;
+        divRef.style.height=`${gridDimension/gridSize}px`;
         gridContainer.appendChild(divRef);
     }   
 }
-gridContainer.style.width=`${gridSize*30}px`;
     
 
